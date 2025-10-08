4ef1a8e6b8973470c7136f38cf86bf09a5d0f2bb
FrancoColombano <francocolombano@gmail.com>
2025-10-07 22:22:19 -0300

    LocalSotorage para carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import {Navigate, Route, Routes } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from "./components/Navigationbar"
-import Carrito from "./pages/Carrito"
-import CarritoOffcanvas from "./components/CarritoOffcanvas"
-import FormularioContacto from "./pages/FormularioContacto"
-import Inicio from "./pages/Inicio"
-import Carta from "./pages/Carta"
-import Footer from "./components/Footer"
-import Nosotros from "./pages/Nosotros"
-import { useState } from "react";
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavigationBar from "./components/Navigationbar";
+import Carrito from "./pages/Carrito";
+import CarritoOffcanvas from "./components/CarritoOffcanvas";
+import FormularioContacto from "./pages/FormularioContacto";
+import Inicio from "./pages/Inicio";
+import Carta from "./pages/Carta";
+import Footer from "./components/Footer";
+import Nosotros from "./pages/Nosotros";
+import { useState, useEffect } from "react";
 
 function App() {
-  
-  const [carro, setCarro] = useState([])
-  const [showCarrito, setShowCarrito] = useState(false) // funciona para decirle al componente de bootstrap offCanvas si se muestra o no
+  const [carro, setCarro] = useState(() => {
+    const carroGuardado = localStorage.getItem("carrito");
+    return carroGuardado ? JSON.parse(carroGuardado) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carro));
+  }, [carro]);
+
+  const [showCarrito, setShowCarrito] = useState(false); // funciona para decirle al componente de bootstrap offCanvas si se muestra o no
 
   const addToCarro = (producto) => {
     setCarro((prevCarro) => {
@@ -36,18 +41,37 @@ function App() {
 
   return (
     <>
-      <NavigationBar totalItems={totalItems} onCarritoClick={() => setShowCarrito(true)} />
-      <CarritoOffcanvas show={showCarrito} handleClose={() => setShowCarrito(false)} carro={carro} setCarro={setCarro} />
+      <NavigationBar
+        totalItems={totalItems}
+        onCarritoClick={() => setShowCarrito(true)}
+      />
+      <CarritoOffcanvas
+        show={showCarrito}
+        handleClose={() => setShowCarrito(false)}
+        carro={carro}
+        setCarro={setCarro}
+      />
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/carta" element={<Carta addToCarro={addToCarro} handleShow={() => setShowCarrito(true) } />} />
-        <Route path="/carrito" element={<Carrito carro={carro} setCarro={setCarro}/>} />
+        <Route
+          path="/carta"
+          element={
+            <Carta
+              addToCarro={addToCarro}
+              handleShow={() => setShowCarrito(true)}
+            />
+          }
+        />
+        <Route
+          path="/carrito"
+          element={<Carrito carro={carro} setCarro={setCarro} />}
+        />
         <Route path="/contacto" element={<FormularioContacto />} />
         <Route path="/nosotros" element={<Nosotros />} />
       </Routes>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
